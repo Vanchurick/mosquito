8249d203a213d5f6582aacf08c7705659d6029e1
Ivan Dashko <davane4ek@gmail.com>
2024-12-10 20:49:32 +0200

    FIX: added eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    plugins: { "react-hooks": pluginReactHooks },
+    rules: {
+      "no-console": "warn",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+];
--- src/components/Selects/DateSelector/Dateselector.js
+++ src/components/Selects/DateSelector/Dateselector.js
@@ -1,5 +1,3 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-
 import React, { useState, useEffect } from "react";
 import { Label, InputDate } from "./StyledComponents";
 
