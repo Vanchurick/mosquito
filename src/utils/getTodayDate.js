6a5b0727473a05da93c36bcb431ce2a5610b228f
Ivan Dashko <davane4ek@gmail.com>
2025-01-01 17:15:57 +0200

    FIX: fixed month value

--- src/utils/getTodayDate.js
+++ src/utils/getTodayDate.js
@@ -2,7 +2,7 @@ const getTodayDate = () => {
 	const today = new Date();
 	const day = today.getDate() < 10 ? `0${today.getDate()}` : today.getDate();
 	const month =
-	  today.getMonth() < 10 ? `0${today.getMonth()}` : today.getMonth() + 1;
+	  today.getMonth() < 10 ? `0${today.getMonth() + 1}` : today.getMonth() + 1;
 	const year = today.getFullYear();
   
 	return `${day}.${month}.${year}`;
